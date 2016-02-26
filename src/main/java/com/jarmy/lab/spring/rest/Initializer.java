c683b9163f8f8f1e776c3d6e1555b9ee6d681ad3
Muhammad Gelbana <m.gelbana@gmail.com>
2016-02-27 01:50:16 +0200

    Add AbstractHttpSessionApplicationInitializer instance

---
+++ src/main/java/com/jarmy/lab/spring/rest/Initializer.java
@@ -0,0 +1,6 @@
+package com.jarmy.lab.spring.rest;
+
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+public class Initializer extends AbstractHttpSessionApplicationInitializer{
+}
